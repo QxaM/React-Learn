2753dae4e3a54f6ba077c1ee872f887bc9855294
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-10-12 13:58:03 +0200

    S17 - continuing working

--- 16-input-forms/src/components/Login.jsx
+++ 16-input-forms/src/components/Login.jsx
@@ -1,17 +1,45 @@
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted");
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            value={email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+            value={password}
+          />
         </div>
       </div>
 
